186dc9dc13ffe855548dbdcf4e5979a495619eb2
razvan neda <neda_razvan@yahoo.com>
2021-02-01 01:59:26 +0000

    crud routes created

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,29 @@
 class TasksController < ApplicationController
+  def index
+      
+  end
+
+  def show
+      
+  end
+
+  def new
+      
+  end
   
+  def create
+
+  end
+  
+  def update 
+
+  end
+  
+  def edit 
+
+  end
+
+  def destroy 
+    
+  end
 end
